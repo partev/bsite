3eb595c47ba8dc799d0b97d1d23be149694a2d60
Simon de Vlieger <simon@ikanobori.jp>
2014-08-11 17:03:14 +0200

    Include a pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'bpython team'
+SITENAME = u'the bpython interpreter'
+SITEURL = ''
+
+RELATIVE_URLS = True
+
+PATH = '.'
+STATIC_PATHS = ['images', 'videos']
+DELETE_OUTPUT_DIRECTORY = True
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+DEFAULT_PAGINATION = False
+
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
